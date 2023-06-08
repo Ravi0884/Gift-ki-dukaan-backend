c7289a613fda31fa9e11479826e1685478cf3e34
Ravi0884 <82750380+Ravi0884@users.noreply.github.com>
2023-06-08 11:49:24 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
-
-const url = process.env.DB_PASSWORD;
-
-module.exports.connect = () =>{
-    mongoose.connect(url).then((res) =>
-     console.log("Db connection successfully")).catch((err)=> console.log('Error:-',err))
-}
